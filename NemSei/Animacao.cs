1357181e7667eeefbea0ae51964225d3f9fdcc77
amabily <amabilymangolin123@gmail.com>
2024-11-06 09:53:17 -0300

    .

---
+++ NemSei/Animacao.cs
@@ -0,0 +1,66 @@
+public class Animacao
+{
+		protected List<String> animacao1 = new List<String>();
+		protected List<String> animacao2 = new List<String>();
+		protected List<String> animacao3 = new List<String>();
+		protected bool loop = true;
+		protected int animacaoAtiva = 1;
+		bool parado = true;
+		int frameAtual = 1;
+		protected Image compImagem;
+		public Animacao(Image a)
+		{
+			compImagem = a;
+		}
+
+	public void Stop()
+	{
+		parado = true;
+	}
+	public void Play()
+	{
+		parado = false;
+	}
+	public void SetAnimacaoAtiva(int a)
+	{
+		animacaoAtiva = a;
+	}
+	public void Desenha()
+	{
+		if(parado)
+		return;
+		string tatu ="";
+		int tamanhoAnimacao = 0;
+		if(animacaoAtiva == 1)
+		{
+			tatu = animacao1[frameAtual];
+			tamanhoAnimacao = animacao1.Count;
+		}
+		else if(animacaoAtiva == 2)
+		{
+			tatu = animacao2 [frameAtual];
+			tamanhoAnimacao = animacao2.Count;
+		}
+		else if(animacaoAtiva == 3)
+		{
+			tatu = animacao3 [frameAtual];
+			tamanhoAnimacao = animacao3.Count;
+		}
+		compImagem.Source = ImageSource.FromFile(tatu);
+		frameAtual++;
+		if (frameAtual >= tamanhoAnimacao)
+		{
+			if (loop)
+			frameAtual = 0;
+			else
+			{
+				parado = true;
+				QuandoParar();
+			}
+		}
+	}
+	public virtual void QuandoParar()
+	{
+		
+	}
+}
--- NemSei/MainPage.xaml.cs
+++ NemSei/MainPage.xaml.cs
@@ -7,5 +7,82 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	
+	int count = 0;
+
+	bool EstaMorto = false;
+	bool EstaPulando = false;
+	const int TempoEntreFrames = 25;
+	int Velocidade1 = 0;
+	int Velocidade2 = 0;
+	int Velocidade3 = 0;
+	int Velocidade = 0;
+	int LarguraJanela = 0;
+	int AlturaJanela = 0;
+
+	protected override void OnSizeAllocated(Double w, Double h)
+	{
+		base.OnSizeAllocated(w,h);
+		CorrigeTamanhoCenario(w,h);
+		CalculaVelocidade(w);
+	}
+
+	void CalculaVelocidade(Double w)
+	{
+		Velocidade1 = (int)(w * 0.001);
+		Velocidade2 = (int)(w * 0.004);
+		Velocidade3 = (int)(w * 0.008);
+		Velocidade = (int)(w * 0.01);
+	}
+
+	void CorrigeTamanhoCenario(Double w, Double h)
+	{
+		foreach (var a in HSLayer1.Children)
+		(a as Image).WidthRequest = w;
+		foreach (var l in HSLayer2.Children)
+		(l as Image).WidthRequest = w;
+		foreach (var k in HSLayer3.Children)
+		(k as Image).WidthRequest = w;
+		foreach (var i in HSLayerChao.Children)
+		(i as Image).WidthRequest = w;
+
+		HSLayer1.WidthRequest = w * 1.5;
+		HSLayer2.WidthRequest = w * 1.5;
+		HSLayer3.WidthRequest = w * 1.5;	
+		HSLayerChao.WidthRequest = w * 1.5;
+	}
+
+	void GerenciaCenarios()
+	{
+		MoveCenario();
+		GerenciaCenario(HSLayer1);
+		GerenciaCenario(HSLayer2);
+		GerenciaCenario(HSLayer3);
+		GerenciaCenario(HSLayerChao);
+
+	}
+
+	void MoveCenario()
+	{
+		HSLayer1.TranslationX -= Velocidade1;
+		HSLayer2.TranslationX -= Velocidade2;
+		HSLayer3.TranslationX -= Velocidade3;
+		HSLayerChao.TranslationX -= Velocidade;
+	}
+
+	void GerenciaCenario(HorizontalStackLayout HSL)
+	{
+		var view = (HSL.Children.First() as Image);
+		if (view.WidthRequest + HSL.TranslationX < 0)
+		{
+			HSL.Children.Remove(view);
+			HSL.Children.Add(view);
+			HSL.TranslationX = view.TranslationX;
+		}
+	}
 }
+ 
+
+ 
 
